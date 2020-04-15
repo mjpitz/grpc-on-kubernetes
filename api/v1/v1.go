bb561475344a75f1d360006a42dcb10d5262934f
Mya Pitzeruse <j.mya.pitz@gmail.com>
2020-04-15 11:48:58 -0500

    added docs to protoc

--- api/v1/v1.go
+++ api/v1/v1.go
@@ -1,3 +1,7 @@
 package v1
 
+// This is using the go generate ./... syntax but this protoc target could be invoked directly.
+// For help getting started with protoc and a specific language, see their tutorials documentation.
+// https://developers.google.com/protocol-buffers/docs/tutorials
+
 //go:generate protoc -I=. -I=$GOPATH/src --gogo_out=plugins=grpc:$GOPATH/src v1.proto
